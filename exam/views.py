3eb8fe510482d26b294ec4d58ac86e4e04119b5b
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2024-03-28 22:01:48 +0530

    use template variable in html file

--- exam/views.py
+++ exam/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def showTest(request):
-    res=render(request,'exam/test.html')
+    que="Who developed C language?"
+    a="ken thompson"
+    b="Danis ritchie"
+    c="Bjarne stroustrup"
+    d="James gosling"
+    data={'que':que,'a':a,'b':b,'c':c,'d':d}
+    res=render(request,'exam/test.html',context=data)
     return res
 def showResult(request):
     s="<h1>This is show result page</h1>"
