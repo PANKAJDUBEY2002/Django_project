aa63879ed00873b0d771662ae4c992f18d6632fd
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2024-03-28 16:00:20 +0530

    add templates MVT followed by design patten

--- exam/views.py
+++ exam/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def showTest(request):
-    s="<h1>This is show test case page</h1>"
-    return HttpResponse(s)
+    res=render(request,'exam/test.html')
+    return res
 def showResult(request):
     s="<h1>This is show result page</h1>"
     return HttpResponse(s)
