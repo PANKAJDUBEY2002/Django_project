86c8c5257982dd963041927641a37b977aee66ac
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2024-03-30 20:49:40 +0530

    add python code to fetch model data

--- testapp/urls.py
+++ testapp/urls.py
@@ -3,6 +3,7 @@ from testapp import views
 
 urlpatterns=[
      path('about',views.about),
+     path('employee',views.employee_info_view),
      path('contact',views.showContact),
      path('',views.greeting),
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from testapp.models import Employee
 # Create your views here.
+def employee_info_view(request):
+    employees=Employee.objects.all()
+    data={'employees':employees}
+    res=render(request,'testapp/employees.html',data)
+    return res
 def greeting(request):
     s="<h1>hello and welcome to first view of testapp</h1>"
     s+="This is landing page"
